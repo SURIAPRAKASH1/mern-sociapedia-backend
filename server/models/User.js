c62c6bc81bd046dc20b6ec6cb317330947cde3f4
SURIAPRAKASH1 <suriaprakashsuri@gmail.com>
2024-07-01 04:38:31 -0700

    create user model with authentication and add public image folder

---
+++ server/models/User.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: [true, "firstname must be provieded !"],
+      min: 2,
+      max: 50,
+    },
+    lastname: {
+      type: String,
+      required: [true, "Lastname must be provided !"],
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "email must be provided !"],
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide a valid email",
+      ],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+      min: 6,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: String,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    {
+      userId: this._id,
+      name: this.firstname,
+    },
+    process.env.JWT_SECRET,
+    {
+      expriresIm: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
+
+const User = mongoose.model("User", UserSchema);
