f65145a5f14299679c57b05bd560bc4b47542cac
SURIAPRAKASH1 <suriaprakashsuri@gmail.com>
2024-07-02 02:27:37 -0700

    storage folder for picture handling

--- server/contollers/auth.js
+++ server/contollers/auth.js
@@ -1,3 +1,5 @@
 import User from "../models/User";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, Unauthenticated } from "../errors";
 
 const register = async (req, res) => {};
---
+++ server/middleware/not-found.js
@@ -0,0 +1,3 @@
+const notfound = (req, res) => res.status(404).send("Route does not exit ");
+
+export default notfound;
