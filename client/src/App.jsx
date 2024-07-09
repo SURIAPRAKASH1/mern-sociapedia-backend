4279f9e55d311c41f975810772cfcf0c9e2535d3
SURIAPRAKASH1 <suriaprakashsuri@gmail.com>
2024-07-09 04:47:05 -0700

    all basic set up and routes , redux state and mui styles

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+import { HomePage, LoginPage, ProfilePage } from "./scenes";
+import { themeSettings } from "./theme";
+
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="home" element={<HomePage />} />
+            <Route path="profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
