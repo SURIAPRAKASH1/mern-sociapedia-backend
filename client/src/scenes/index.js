f21cd33c617129021c3c535d1d61930e6a7370b2
SURIAPRAKASH1 <148183120+SURIAPRAKASH1@users.noreply.github.com>
2024-07-16 00:41:39 -0700

    Delete client directory

--- client/src/scenes/index.js
+++
@@ -1,5 +0,0 @@
-export { default as HomePage } from "./homePage";
-export { default as LoginPage } from "./loginPage";
-
-export { default as ProfilePage } from "./profilePage";
-export { default as Navbar } from "./navbar";
